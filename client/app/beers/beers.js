9328a0af997cee1a68ec836631d722f6909b8ae2
Boya Jiang <jiang.boya@yahoo.com>
2016-02-14 22:55:35 -0800

    Add a test case in the $scope

--- client/app/beers/beers.js
+++ client/app/beers/beers.js
@@ -1,4 +1,9 @@
 angular.module('beer.beers', [])
 .controller('BeersController', function ($scope) {
-
+	    $scope.products = ['Math', 'Physics', 'Chemistry', 'Hindi', 'English'];
+ 
+    $scope.dropboxitemselected = function () {
+    	console.log("function called!");
+        alert("drop box item selected");
+    }
 });
