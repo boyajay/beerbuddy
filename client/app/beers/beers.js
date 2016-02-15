e33ddd2ecab45114875a789cfa9a583b58e44f90
Boya Jiang <jiang.boya@yahoo.com>
2016-02-15 15:10:51 -0800

    Select menu 1 now populates from controller's selection1 property

--- client/app/beers/beers.js
+++ client/app/beers/beers.js
@@ -1,6 +1,12 @@
 angular.module('beer.beers', [])
 .controller('BeersController', function ($scope) {
 	    $scope.products = ['Math', 'Physics', 'Chemistry', 'Hindi', 'English'];
+	   $scope.test = 'test';
+	  $scope.selection1 =[
+      {path: '/category', name: 'by beer type'},
+      {path: '/location/region', name: 'by region'},
+      {path: '/breweries', name: 'by brewery'}
+    ];
  
     $scope.dropboxitemselected = function () {
     	console.log("function called!");
