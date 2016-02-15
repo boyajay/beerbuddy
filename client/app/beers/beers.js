9ff9a1040a01448898bcb5b43f0ce7e8f4fab698
Boya Jiang <jiang.boya@yahoo.com>
2016-02-15 15:49:18 -0800

    Clean up test cases from code

--- client/app/beers/beers.js
+++ client/app/beers/beers.js
@@ -1,7 +1,5 @@
 angular.module('beer.beers', [])
 .controller('BeersController', function ($scope) {
-	    $scope.products = ['Math', 'Physics', 'Chemistry', 'Hindi', 'English'];
-	   $scope.test = 'test';
 	  $scope.selection1 =[
       {path: '/category', name: 'by beer type'},
       {path: '/location/region', name: 'by region'},
