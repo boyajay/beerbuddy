4f2ba8f1e885fd1b6187e74bc14e35df10b5f832
Boya Jiang <jiang.boya@yahoo.com>
2016-02-15 19:14:53 -0800

    Add jquery and use for ajax requests

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,21 +2,33 @@ angular.module('beer.services', [])
 .factory('BeerLists', function ($http) {
   var getList = function (path){
     path = path || '';
-    $http({
-      method: 'GET',
-      url: 'http://api.brewerydb.com/v2'+path+'/?key=31b3b48826470b8ee3ca36aabb7560ab'
-      // key: '31b3b48826470b8ee3ca36aabb7560ab'
-    }).then(function successCallback(response) {
-        console.log("GREAT SUCCESS!!");
-        return response;
-        // this callback will be called asynchronously
-        // when the response is available
-      }, function errorCallback(err) {
-        console.log("ajax call failed");
-        return err;
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
+    $.ajax({
+      url: 'http://api.brewerydb.com/v2'+path+'/?key=31b3b48826470b8ee3ca36aabb7560ab',
+      type: 'GET',
+      success: function(data) {
+        // Don't bother if we have nothing to work with
+        console.log(data);
+      },
+      error: function(data) {
+        console.error('Failed to fetch beers');
+      }
+    });
+
+    // $http({
+    //   method: 'GET',
+    //   url: 'http://api.brewerydb.com/v2'+path+'/?key=31b3b48826470b8ee3ca36aabb7560ab'
+    //   // key: '31b3b48826470b8ee3ca36aabb7560ab'
+    // }).then(function successCallback(response) {
+    //     console.log("GREAT SUCCESS!!");
+    //     return response;
+    //     // this callback will be called asynchronously
+    //     // when the response is available
+    //   }, function errorCallback(err) {
+    //     console.log("ajax call failed");
+    //     return err;
+    //     // called asynchronously if an error occurs
+    //     // or server returns response with an error status.
+    //   });
   }
   return {
     getList: getList
