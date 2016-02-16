bb4398e40e884c9b013f65baa8dcb4f091a03009
Boya Jiang <jiang.boya@yahoo.com>
2016-02-16 11:12:15 -0800

    Fixed async ajax call returning proper value

--- server/users/userController.js
+++ server/users/userController.js
@@ -4,6 +4,7 @@ var User = require('./userModel.js');
 var http = require('http');
 var BreweryDb = require('brewerydb-node');
 var brewdb = new BreweryDb('31b3b48826470b8ee3ca36aabb7560ab');
+var request = require('request');
 
 // Promisify a few mongoose methods with the `q` promise library
 var findUser = Q.nbind(User.findOne, User);
@@ -11,18 +12,22 @@ var createUser = Q.nbind(User.create, User);
 
 module.exports = {
   getBeerCats: function(req, res, next){
-    var result = brewdb.category.all(function (err, result){
-      return res.json(result);
-    });
-  }
-  //   http.get("http://api.brewerydb.com/v2/categories?key=31b3b48826470b8ee3ca36aabb7560ab", function(res) {
-  //     //console.log(res);
-  //     dataObj= res;
-  //     console.log("passed in object is " , dataObj);
-  //     console.log("Got response: " + res.statusCode);
-  //   }).then(function(val) {
-  //     console.log("getBeer occured, value is ", val);
-  //     return val;
+  //   var result = brewdb.category.all(function (err, result){
+  //     return res.json(result);
   //   });
   // }
-};
+
+    var url = "http://api.brewerydb.com/v2" + req.query.params + "?key=31b3b48826470b8ee3ca36aabb7560ab"
+    console.log("url::::::::::: ", url );
+    request(url, function(error, response, body) {
+      //console.log(res);
+      console.log(body);
+      res.json(body);
+    });
+    // .then(function(val) {
+    //   console.log("getBeer occured, value is ", val);
+    //   res.json(val);
+    // });
+  // }
+  }
+} 
