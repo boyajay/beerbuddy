51ee16990b799fd3ef9d512f13e9e2b04e20fa1c
Boya Jiang <jiang.boya@yahoo.com>
2016-02-21 18:59:17 -0800

    Hook up myLists with factory  methods

--- client/app/mylists/mylists.js
+++ client/app/mylists/mylists.js
@@ -1,7 +1,15 @@
 angular.module('beer.mylists', [])
 
-.controller('ListsController', function ($scope, Data) {
+.controller('ListsController', function ($scope, $window, BeerLists) {
 
-	$scope.data = Data;
+	$scope.lists = {};	
+  $scope.getMyLists = function(){
+    var user = $window.localStorage.user;
+    BeerLists.getLists(user,function(resp){
+      console.log('resp to getMyLists is ', resp);
+      $scope.lists.favorites = resp.favorites; //JSON.parse(resp.data)
+      $scope.lists.favorites = resp.wishlist;
+    });
+  }
 
 });
